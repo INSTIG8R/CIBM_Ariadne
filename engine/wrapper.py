93b70234ad558e5b476c8b21d5dc2176749f5332
INSTIG8R <ahammedsakir99@gmail.com>
2024-06-24 14:28:33 +0600

    training with swin unetr default

---
+++ engine/wrapper.py
@@ -0,0 +1,167 @@
+from utils.model import LanGuideMedSeg
+from utils.swin_unetr import SwinUNETR
+from monai.losses import DiceCELoss
+from torchmetrics import Accuracy,Dice
+from torchmetrics.classification import BinaryJaccardIndex
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from copy import deepcopy
+import pandas as pd
+import sys
+import numpy as np
+import datetime
+
+class LanGuideMedSegWrapper(pl.LightningModule):
+
+    def __init__(self, args):
+        
+        super(LanGuideMedSegWrapper, self).__init__()
+        
+        # self.model = LanGuideMedSeg(args.bert_type, args.vision_type1, args.project_dim)
+        self.model = SwinUNETR(in_channels=3,
+                           out_channels=9,
+                           img_size=args.image_size,
+                           feature_size=48,
+                           norm_name='batch',
+                           spatial_dims=2)  #using unetr model
+        self.lr = args.lr
+        self.history = {}
+        
+        # self.loss_fn = DiceCELoss(include_background=False,softmax=True,to_onehot_y=True)  #without bg
+
+        self.loss_fn = DiceCELoss(include_background=True,softmax=True,to_onehot_y=True,lambda_dice=0.6,lambda_ce=0.4)        
+
+        # metrics_dict = {"acc":Accuracy(task='binary'),"dice":Dice(),"MIoU":BinaryJaccardIndex()}
+        # metrics_dict = {"dice_micro":Dice(average='micro',ignore_index=0),"dice_macro":Dice(average='macro',num_classes=9,ignore_index=0),
+        #                 "dice_class0":Dice(average='none',num_classes=9,ignore_index=0)[0],
+        #                 "dice_class1":Dice(average='none',num_classes=9,ignore_index=0)[1],
+        #                 "dice_class2":Dice(average='none',num_classes=9,ignore_index=0)[2],
+        #                 "dice_class3":Dice(average='none',num_classes=9,ignore_index=0)[3],
+        #                 "dice_class4":Dice(average='none',num_classes=9,ignore_index=0)[4],
+        #                 "dice_class5":Dice(average='none',num_classes=9,ignore_index=0)[5],
+        #                 "dice_class6":Dice(average='none',num_classes=9,ignore_index=0)[6],
+        #                 "dice_class7":Dice(average='none',num_classes=9,ignore_index=0)[7],
+        #                 "dice_class8":Dice(average='none',num_classes=9,ignore_index=0)[8]
+        #                 }  #Without bg
+
+        metrics_dict = {"dice_micro":Dice(average='micro'),"dice_macro":Dice(average='macro',num_classes=9),
+                        "dice_class0":Dice(average='none',num_classes=9)[0],
+                        "dice_class1":Dice(average='none',num_classes=9)[1],
+                        "dice_class2":Dice(average='none',num_classes=9)[2],
+                        "dice_class3":Dice(average='none',num_classes=9)[3],
+                        "dice_class4":Dice(average='none',num_classes=9)[4],
+                        "dice_class5":Dice(average='none',num_classes=9)[5],
+                        "dice_class6":Dice(average='none',num_classes=9)[6],
+                        "dice_class7":Dice(average='none',num_classes=9)[7],
+                        "dice_class8":Dice(average='none',num_classes=9)[8]
+                        }  #Without bg
+
+        self.train_metrics = nn.ModuleDict(metrics_dict)
+        self.val_metrics = deepcopy(self.train_metrics)
+        self.test_metrics = deepcopy(self.train_metrics)
+        
+        self.save_hyperparameters()
+
+    def configure_optimizers(self):
+
+        optimizer = torch.optim.AdamW(self.model.parameters(),lr = self.lr)
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max =200, eta_min=1e-6)
+
+        return {"optimizer":optimizer,"lr_scheduler":lr_scheduler}
+        
+    def forward(self,x):
+       
+       return self.model.forward(x)
+
+
+    def shared_step(self,batch,batch_idx):
+        x, y = batch
+        preds = self(x)
+        loss = self.loss_fn(preds,y)
+        return {'loss': loss, 'preds': preds.detach(), 'y': y.detach()}
+    
+    def training_step(self, batch, batch_idx):
+        return self.shared_step(batch,batch_idx)
+    
+    def validation_step(self, batch, batch_idx):
+        return self.shared_step(batch,batch_idx)
+    
+    def test_step(self, batch, batch_idx):
+        return self.shared_step(batch,batch_idx)
+    
+    def predict_step(self, batch, batch_idx):
+        if isinstance(batch,list) and len(batch)==2:
+            return self(batch[0])
+        else:
+            return self(batch)
+        
+    def shared_step_end(self,outputs,stage):
+        metrics = self.train_metrics if stage=="train" else (
+            self.val_metrics if stage=="val" else self.test_metrics)
+        for name in metrics:
+            step_metric = metrics[name](outputs['preds'], outputs['y']).item()
+            if stage=="train":
+                self.log(name,step_metric,prog_bar=True)
+        return outputs["loss"].mean()
+        
+    def training_step_end(self, outputs):
+        return {'loss':self.shared_step_end(outputs,"train")}
+            
+    def validation_step_end(self, outputs):
+        return {'val_loss':self.shared_step_end(outputs,"val")}
+            
+    def test_step_end(self, outputs):
+        return {'test_loss':self.shared_step_end(outputs,"test")}
+            
+    def shared_epoch_end(self,outputs,stage="train"):
+        metrics = self.train_metrics if stage=="train" else (
+            self.val_metrics if stage=="val" else self.test_metrics)
+        
+        epoch = self.trainer.current_epoch
+        stage_loss = torch.mean(torch.tensor([t[(stage+"_loss").replace('train_','')] for t in outputs])).item()
+        dic = {"epoch":epoch,stage+"_loss":stage_loss}
+        
+        for name in metrics:
+            epoch_metric = metrics[name].compute().item() 
+            metrics[name].reset()
+            dic[stage+"_"+name] = epoch_metric 
+        if stage!='test':
+            self.history[epoch] = dict(self.history.get(epoch,{}),**dic)    
+        return dic 
+    
+    def training_epoch_end(self, outputs):
+        dic = self.shared_epoch_end(outputs,stage="train")
+        self.print(dic)
+        dic.pop("epoch",None)
+        self.log_dict(dic, logger=True)
+
+    def validation_epoch_end(self, outputs):
+        dic = self.shared_epoch_end(outputs,stage="val")
+        self.print_bar()
+        self.print(dic)
+        dic.pop("epoch",None)
+        self.log_dict(dic, logger=True)
+        
+        #log when reach best score
+        ckpt_cb = self.trainer.checkpoint_callback
+        monitor = ckpt_cb.monitor
+        mode = ckpt_cb.mode
+        arr_scores = self.get_history()[monitor]
+        best_score_idx = np.argmax(arr_scores) if mode=="max" else np.argmin(arr_scores)
+        if best_score_idx==len(arr_scores)-1:   
+            self.print("<<<<<< reach best {0} : {1} >>>>>>".format(
+                monitor,arr_scores[best_score_idx]),file = sys.stderr)
+    
+    def test_epoch_end(self, outputs):
+        dic = self.shared_epoch_end(outputs,stage="test")
+        dic.pop("epoch",None)
+        self.print(dic)
+        self.log_dict(dic, logger=True)
+        
+    def get_history(self):
+        return pd.DataFrame(self.history.values()) 
+    
+    def print_bar(self): 
+        nowtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.print("\n"+"="*80 + "%s"%nowtime)
